5736b10519e32cc319f8b9b6f80516d4a86ae944
Aleksandr Noskov <nemestny@gmail.com>
2019-01-21 14:49:27 +0500

    Add sin wave

--- sound_engine.ino
+++ sound_engine.ino
@@ -9,10 +9,10 @@
 */
 #include <PWM.h>
 int speaker = 9; // пин, светодиод
-int frequency_min = 1; // частота  1 - 2000000 (Гц)  минимальная частота
-int frequency_max = 300; //максимальная частота
+int frequency_min = 31; // частота  1 - 2000000 (Гц)  минимальная частота
+int frequency_max = 150; //максимальная частота
 int level_min = 0;   // частота ШИМ (0-255) минимальная громкость
-int level_max = 40; // максимальная громкость
+int level_max = 200; // максимальная громкость
 int frequency = frequency_min; //текущая частота
 int level = level_min; //текущая громкость
 int input = A0; // вход для управления
@@ -20,7 +20,7 @@ int input = A0; // вход для управления
 void setup() {
   InitTimersSafe();                         //инициализируем все таймеры, кроме 0,
   //SetPinFrequencySafe(speaker, frequency);    //устанавливает частоту для указанного pin
-  SetPinFrequency(speaker, frequency);          //устанавливает частоту для указанного pin
+  SetPinFrequencySafe(speaker, frequency);          //устанавливает частоту для указанного pin
   pinMode(input, INPUT); //вход для управления
 //  TCCR1A = TCCR1A & 0xe0 | 3;
 //  TCCR1B = TCCR1B & 0xe0 | 0x0c; 
@@ -34,28 +34,31 @@ void loop() {
   }
 */
   if (digitalRead(input)==HIGH){
-      level += 1; //увеличение громкости
-      frequency +=5; //увеличение частоты
+      level +=5; //увеличение громкости
+      frequency +=1; //увеличение частоты
       if (level > level_max) {
           level = level_max;
         }
+ //     if (level == 255) {level = 257;}
       if (frequency > frequency_max){
           frequency = frequency_max;
         }
     } else {
-      level -= 1; //уменьшение громкости
-      frequency -=5; //уменьшение частоты
+      level -=5; //уменьшение громкости
+      frequency -=1; //уменьшение частоты
       if (level < level_min) {
           level = level_min;
         }
+
       if (frequency < frequency_min){
           frequency = frequency_min;
+          level = level_min;
         }
       }
 
-  SetPinFrequency(speaker, frequency);
+  SetPinFrequencySafe(speaker, frequency);
   pwmWriteHR(speaker, level);     //0-255   используйте эту функцию вместо analogWrite
-  delay(100);//задержка цикла
+  delay(12);//задержка цикла
   //или
   //pwmWriteHR(speaker, levelHR); //0-65536 используйте эту функцию вместо analogWrite
   //  pwmWrite(speaker,0);
